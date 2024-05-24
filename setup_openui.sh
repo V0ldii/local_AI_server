b16ef9e0e654c12fbc0eee4b5fb7ea552dc56a67
Annu Jangra <126829312+Volemort79219@users.noreply.github.com>
2024-05-25 00:07:14 +0530

    setup_openui.sh

---
+++ setup_openui.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Pull the OpenUI Docker image and run it
+sudo docker run -d --network=host -v open-webui:/app/backend/data -e OLLAMA_BASE_URL=http://127.0.0.1:11434 --name open-webui --restart always ghcr.io/open-webui/open-webui:main
