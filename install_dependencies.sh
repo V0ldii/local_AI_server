b9f4a958c7d164addcd31ccc16b33971c023f368
Annu Jangra <126829312+Volemort79219@users.noreply.github.com>
2024-05-25 00:03:48 +0530

    install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update
+
+# Install build dependencies
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
+python-openssl git
